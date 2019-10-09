f2aae1405108d8a9e715689e323b499c09eea2c8
cjkuhlmann <56342962+cjkuhlmann@users.noreply.github.com>
2019-10-09 17:40:55 +0100

    Update roverVar.py

--- roverVar.py
+++ roverVar.py
@@ -1,3 +1,5 @@
+#---------------------------Standardied variable type containing network identifier and value--------------------
+#ALL ROVER VALUES MUST BE INTEGER OR FLOAT IN ORDER TO ALLOW COMPARISON UNLESS COMPARATOR IS CHANGED
 class roverVar():
     def __init__(self,val,name):
         self.value = val
