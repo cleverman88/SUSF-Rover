ab0ee5db775459726809d8c606ad0fdbb31bb3ca
cjkuhlmann <56342962+cjkuhlmann@users.noreply.github.com>
2019-10-09 17:38:06 +0100

    Update arm.py

--- arm.py
+++ arm.py
@@ -1,8 +1,9 @@
 from roverVar import *
-
+#--------------------------------example of arm variable--------------------------------------
 class arm():
     def __init__(self):
         self.grabberPower = roverVar(0,"gp")
         self.armX = roverVar(0,"ax")
         self.armY = roverVar(0,"ay")
         self.armZ = roverVar(0,"az")
+        
